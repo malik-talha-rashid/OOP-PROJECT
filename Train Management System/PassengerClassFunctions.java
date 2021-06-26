7b1d294f6cae3d353205bf7cb93032b3b41e4f5f
malik-talha-rashid <80753851+malik-talha-rashid@users.noreply.github.com>
2021-06-26 23:44:39 +0500

    Add files via upload

---
+++ Train Management System/PassengerClassFunctions.java
@@ -0,0 +1,7 @@
+package a1Proj2;
+
+public interface PassengerClassFunctions {
+    void displayBookedTickets();
+    void displayAllTickets();
+    void displayAvailableTickets();
+}
---
+++ Train Management System/TicketFunctions.java
@@ -0,0 +1,8 @@
+package a1Proj2;
+
+public interface TicketFunctions {
+
+    boolean isBooked();
+    void book(String passengerName);
+    void unBook();
+}
