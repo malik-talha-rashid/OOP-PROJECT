31864ddf4b1f275a1d448bc15fa31fcbe75dddb8
malik-talha-rashid <80753851+malik-talha-rashid@users.noreply.github.com>
2021-06-27 15:46:04 +0500

    Delete Train Management System directory

--- Train Management System/PassengerClassFunctions.java
+++
@@ -1,7 +0,0 @@
-package a1Proj2;
-
-public interface PassengerClassFunctions {
-    void displayBookedTickets();
-    void displayAllTickets();
-    void displayAvailableTickets();
-}
--- Train Management System/TicketFunctions.java
+++
@@ -1,8 +0,0 @@
-package a1Proj2;
-
-public interface TicketFunctions {
-
-    boolean isBooked();
-    void book(String passengerName);
-    void unBook();
-}
